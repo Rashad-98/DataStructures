e808aa98e4a7e4d5505f6cb6300806047c7697dd
Abdelrahman_Rashad <a.rashaad98@gmail.com>
2023-01-07 09:42:39 +0200

    reafactor the recursive traverser function to make it a near-pure function

--- BinarySearchTree.mjs
+++ BinarySearchTree.mjs
@@ -11,49 +11,38 @@ class BinarySearchTree {
         this.root = null;
     }
 
-    #recursiveTraverser (rootNode, newNode) {
-        if(newNode.value > rootNode.value){
-           if(rootNode.right === null) {
-                return rootNode.right = newNode;
-           }
-           rootNode = rootNode.right; 
-           this.#recursiveTraverser(rootNode, newNode);
-        }
-        if(newNode.value < rootNode.value) {
-            if(rootNode.left === null) {
-                return rootNode.left = newNode;
+    #recursiveTraverser (root, newNode) {
+
+        const traverserRoot = root;
+
+        (function traverser(traverserRoot, newNode) {
+            if((newNode.value > traverserRoot.value) && (traverserRoot.right === null)){
+                return traverserRoot.right = newNode;
+            } else if((newNode.value > traverserRoot.value)) {
+                traverserRoot = traverserRoot.right; 
+                traverser(traverserRoot, newNode);
             }
-            rootNode = rootNode.left;
-            this.#recursiveTraverser(rootNode, newNode);
-        }
+            if((newNode.value < traverserRoot.value) && (traverserRoot.left === null)) {
+                return traverserRoot.left = newNode;
+            } else if((newNode.value < traverserRoot.value)) {
+                traverserRoot = traverserRoot.left;
+                traverser(traverserRoot, newNode);
+            }
+        })(traverserRoot, newNode)
     }
 
     insert (value) {
         const node = new Node(value);
         if(this.root === null) return this.root = node;
-        let ourRoot = this.root;
-        this.#recursiveTraverser(ourRoot, node);
+        this.#recursiveTraverser(this.root, node);
     }
 }
 
 
 // tests
-const tree = new BinarySearchTree();
-tree.insert(5);
-tree.insert(7);
-tree.insert(3);
-tree.insert(4);
-console.log(tree);
-
-// const newThing = new BinarySearchTree();
-// newThing.hello();
-
-
-
-
-//insert
-// take the value and make a node with it
-// if the tree is empty, set the root to the newly created node
-// make a new variable called ourRoot and make it point to this.root
-// make a recursive function that checks if ourRoot has a value greater or smaller that the new value, if greater (add the samller part),
-// check ourRoot's right, if it is null set it to the new value and retur, and if it is not null call that recursive function again.
+// const tree = new BinarySearchTree();
+// tree.insert(5);
+// tree.insert(7);
+// tree.insert(3);
+// tree.insert(4);
+// console.log(tree);
